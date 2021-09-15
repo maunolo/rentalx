2fddfe690906329c5390a34597fceb9dd9a80657
Mauricio Lopes <mauricionlopes.dev@gmail.com>
2021-09-15 18:23:12 -0300

    finishing categories and specifications endpoints

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { CreateSpecificationUseCase } from './CreateSpecificationUseCase'
+
+class CreateSpecificationController {
+  constructor(private createSpecificationUseCase: CreateSpecificationUseCase) {}
+
+  handle(request: Request, response: Response) {
+    try {
+      const { name, description } = request.body
+
+      this.createSpecificationUseCase.execute({ name, description })
+
+      return response.status(201).send()
+    } catch (error) {
+      response.status(422).json({ error: error.message })
+    }
+  }
+}
+
+export { CreateSpecificationController }
